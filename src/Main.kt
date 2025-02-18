d04d75e18f50bd437b4fec5b8cf67daaa237aade
Bader Alshammari <q8.badoori.11@gmail.com>
2025-02-18 19:27:08 +0300

    enums

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
+enum class UserRole(val accessLevel: Int) {
+    ADMIN(1),
+    EDITOR(2),
+    VIEWER(3)
+}
+
+fun checkUserRole(role: UserRole, level: Int): Boolean {
+    return role.accessLevel == level
+}
+
 fun main() {
-    println("Hello World!")
+    val bader = UserRole.ADMIN
+    println(checkUserRole(bader, 2))
+
+    println("\nAll Roles:")
+    for (role in UserRole.entries) {
+        println("${role.name} has access level: ${role.accessLevel}")
+    }
 }
